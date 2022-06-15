088a3d29b52c707563e75c285e291d27e0328c79
Chinami Miyashita <106375669+Natty0404@users.noreply.github.com>
2022-06-15 23:15:20 +0900

    [Finish] topページ

--- bookers2/config/routes.rb
+++ bookers2/config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
 root to: "homes#top"
 devise_for :users
 
 resources :books, only: [:new, :create, :index, :show]
-resources :users, only: [:show, :edit]
+resources :users, only: [:show, :edit, :update]
 get '/homes/about' => 'homes#about', as: 'about'
 
 end
