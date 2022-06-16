12ffc4aafa95c5b00307eb8a138aaa3ddb8b9554
Chinami Miyashita <106375669+Natty0404@users.noreply.github.com>
2022-06-16 16:31:57 +0900

    [Finish] user/show

--- bookers2/app/controllers/users_controller.rb
+++ bookers2/app/controllers/users_controller.rb
@@ -2,9 +2,10 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @books = @user.books
+    @book = Book.new
   end
 
-  def  edit
+  def edit
     @user = User.find(params[:id])
   end
 
