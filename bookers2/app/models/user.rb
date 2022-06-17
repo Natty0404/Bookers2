9ea6ba8327dbc8b201fbdc3b7c9fd7c24795b3b4
Chinami Miyashita <106375669+Natty0404@users.noreply.github.com>
2022-06-17 17:54:00 +0900

    課題3 バリデーションから

--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
+  validates :name, presence: false, length: { minimum: 2, maximum: 20 }
+  validates :email, presence: true
   validates :introduction, presence: false, length: { maximum: 50 }
 
   def get_profile_image(width, height)
