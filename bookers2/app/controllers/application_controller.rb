998784879ac5b39d3ec490d58bf3a3db0c0d2909
Chinami Miyashita <106375669+Natty0404@users.noreply.github.com>
2022-06-20 02:04:22 +0900

    課題3 エラーチェック中

--- bookers2/app/controllers/application_controller.rb
+++ bookers2/app/controllers/application_controller.rb
@@ -1,13 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:top]
-
+  before_action :authenticate_user!, except: [:top, :about]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
     def after_sign_up_path_for(resource)
-      if current_user
-        flash[:success] = "Welcome! You have signed up successfully."
-        user_path(resource)
-      end
+      flash[:success] = "Welcome! You have signed up successfully."
+      user_path(resource)
     end
 
     def after_sign_in_path_for(resource)
