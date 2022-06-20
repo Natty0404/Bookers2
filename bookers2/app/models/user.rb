7b125cb417081b6fccece074587f81f5341b973c
Chinami Miyashita <106375669+Natty0404@users.noreply.github.com>
2022-06-20 16:13:53 +0900

    課題3 部分テンプレート完成

--- bookers2/app/models/user.rb
+++ bookers2/app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
 
-  validates :name, presence: false, length: { minimum: 2, maximum: 20 }
+  validates :name, presence: false, uniqueness: true, length: { minimum: 2, maximum: 20 }
   validates :email, presence: true
   validates :introduction, presence: false, length: { maximum: 50 }
 
